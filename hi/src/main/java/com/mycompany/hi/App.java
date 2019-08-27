b1e783068afff2691c459b23cc2f4ebc7e0ef83e
grantnagy <grantnagy11@gmail.com>
2019-08-27 12:08:15 -0600

    sout hi

---
+++ hi/src/main/java/com/mycompany/hi/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hi;
+
+/**
+ *
+ * @author Grant
+ */
+public class App {
+    public static void main(String [] args) throws Exception {
+        App app = new App();
+        app.run();
+    }
+
+    private void run() {
+        System.out.println("yooo");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
