976edf68dada2dead575913192def2f85eff6403
grantnagy <grantnagy11@gmail.com>
2019-09-12 11:14:09 -0600

    hangman kattis start. declared variables.

---
+++ hangman/src/main/java/com/mycompany/hangman/App.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.hangman;
+
+/**
+ *byte
+ * char
+ * short
+ * mt
+ * long
+ * float
+ * double
+ * 
+ */
+public class App {
+   
+public static void main(String[] args) throws Exception{
+    
+    int count = 0;
+    char[] input;
+    for (int i = 0; count < 10)
+    
+}
+private void run()
+{
+    throw new UnsupportedOperationException("Not supported yet.");
+}
+}
---
+++ hangman/src/test/java/com/mycompany/hangman/AppTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hangman;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ *
+ * @author Grant
+ */
+public class AppTest {
+    
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
