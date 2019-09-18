96e67d3fae42b2b4febc7bdba0908279fe584b79
grantnagy <grantnagy11@gmail.com>
2019-09-18 12:06:02 -0600

    No errors

--- JobExpenses/src/main/java/edu/coloradomesa/jobexpenses/App.java
+++ JobExpenses/src/main/java/edu/coloradomesa/jobexpenses/App.java
@@ -6,49 +6,49 @@
 package edu.coloradomesa.jobexpenses;
 import java.io.PrintStream;
 import java.util.Scanner;
-import java.io.*;
-import java.util.*;
 /**
  *
  * @author Grant
  */
 public class App {
+    Scanner in = new Scanner(System.in);
+    PrintStream out = System.out;
+    int a,b,c,d,e, counter = 0;
+    int expenses = 0;
     
-    int a,b,c,d;
-    int expenses;
-    
-    
-    public static void main(String[] args) throws Exception {
-        App app = new App();
-        app.run();
-    }
-    
-    void run()
-    {
-        read();
-        solve();
-        write();
-    }
-    
-    void write()
-    {
-       out.println(expenses);
-    }
-    
-    
-    void read() {
-        a = in.nextInt();
-        b = in.nextInt();
-        c = in.nextInt();
-        d = in.nextInt();
-        
-    }
-    
-    
- void solve () {
-     
-      
+ public static void main(String[] args) throws Exception {   
+ App app = new App();
+ app.run();
  }
-     
-    
+ 
+  void run()
+  {
+ 
+  while (counter < 10)
+  {
+       a = in.nextInt();
+       if (a < 0)
+         expenses += -a;
+       
+        b = in.nextInt();
+       if (b < 0)
+         expenses += -b;
+       
+        c = in.nextInt();
+       if (c < 0)
+         expenses += -c;
+       
+        d = in.nextInt();
+       if (d < 0)
+         expenses += -d;
+       
+        e = in.nextInt();
+       if (e < 0)
+         expenses += -e;
+       
+       
+       counter++;
+     }
+  out.println(expenses);
+    }
 }
--- JobExpenses/src/test/java/edu/coloradomesa/jobexpenses/AppTest.java
+++ JobExpenses/src/test/java/edu/coloradomesa/jobexpenses/AppTest.java
@@ -23,16 +23,96 @@ public class AppTest {
         return app;
     }
     
-    
-    
-    
+    App ex2() {
+        App app = new App();
+        app.a = 5;
+        app.b = 10;
+        app.c = -2;
+        app.d = -2;
+        app.e = 21;
+        app.expenses = 4;
+        return app;
+    }
+
+    App ex3() {
+        App app = new App();
+        app.a = 100;
+        app.b = 1000;
+        app.c = -100;
+        app.d = 50;
+        app.e = -2;
+        app.expenses = 102;
+        return app;
+    }
+   
+
+    App ex(int k) {
+        switch (k) {
+            case 1:
+                return ex1();
+            case 2:
+                return ex2();
+            case 3:
+                return ex3();
+        }
+        throw new IndexOutOfBoundsException("k=" + k);
+    }
+
+    void assertAppInEquals(App expect, App result) {
+        assertEquals(expect.a, result.a);
+        assertEquals(expect.b, result.b);
+        assertEquals(expect.c, result.c);
+        assertEquals(expect.d, result.d);
+        assertEquals(expect.e, result.e);
+    }
+
+    void assertAppOutEquals(App expect, App result) {
+        assertEquals(expect.expenses, result.expenses);
+    }
+
+    public void testSample(int testcase) {
+        App expected = ex(testcase);
+        App result = ex(testcase);
+        result.expenses = 0;
+        assertAppOutEquals(expected, result);
+    }
+
+    @Test
+    public void testSample1() {
+        testSample(1);
+    }
+
+    @Test
+    public void testSample2() {
+        testSample(2);
+    }
+
+    @Test
+    public void testSample3() {
+        testSample(3);
+    }
+
+
+    @Test
+    public void testWrite3() {
+        String expect = "2" + System.lineSeparator();
+        App app = new App();
+        var bos = new ByteArrayOutputStream();
+        app.out = new PrintStream(bos);
+        app.expenses = 2;
+        app.out.close();
+        String result = bos.toString();
+        assertEquals(expect, result);
+    }
+
+
     @Test
     public void testRun() {
         App app = new App();
         
         String EOL = System.lineSeparator();
         app.in = new Scanner("3" + EOL
-                + "1" + "-2" + "3"+ EOL);
+                + "1 -2 3" + EOL);
         var bos = new ByteArrayOutputStream();
         app.out = new PrintStream(bos);
         app.run();
@@ -41,14 +121,4 @@ public class AppTest {
         String result = bos.toString();
         assertEquals(expect, result);
     }
-   /** @Test
-    public void testGetDefaultWho()
-    {
-        System.out.println("getWho");
-        App instance = new App();
-        String expResult = "World";
-        String result = instance.getWho();
-        assertEquals(expResult, result);
-    }
-    * **/
 }
