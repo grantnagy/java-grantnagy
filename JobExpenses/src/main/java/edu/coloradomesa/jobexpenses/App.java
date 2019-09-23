1b91846f0855d57de2889beb19934b1837d5c631
grantnagy <grantnagy11@gmail.com>
2019-09-23 12:12:18 -0600

    trying bufferedreader instead

--- JobExpenses/src/main/java/edu/coloradomesa/jobexpenses/App.java
+++ JobExpenses/src/main/java/edu/coloradomesa/jobexpenses/App.java
@@ -4,68 +4,31 @@
  * and open the template in the editor.
  */
 package edu.coloradomesa.jobexpenses;
-import java.io.PrintStream;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Grant
  */
 public class App {
-    Scanner in = new Scanner(System.in);
-    PrintStream out = System.out;
-    int a,b,c,d,e, counter = 0;
-    int expenses;
-    
- public static void main(String[] args) throws Exception {   
- App app = new App();
- app.run();
- }
- 
-  void run()
-  {
-      while (true) {
-      solve();
-      if (finished()) break;
-      print();
-      }
-  }
- 
-  void solve() 
-  {    
-  while (counter < 5)
-  {
-       a = in.nextInt();
-       if (a < 0)
-         expenses += -a;
-       
-        b = in.nextInt();
-       if (b < 0)
-         expenses += -b;
-       
-        c = in.nextInt();
-       if (c < 0)
-         expenses += -c;
-       
-        d = in.nextInt();
-       if (d < 0)
-         expenses += -d;
-       
-        e = in.nextInt();
-       if (e < 0)
-         expenses += -e;
-       
-       
-       counter++;
-     }
-  }
-  
-  boolean finished() 
-    {
-        return (a == 0 && b == 0 && c == 0 && d == 0 && e == 0);
-    }
-  
-void print()
-    {
-  out.println(expenses);
-    }
-}
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int a, expenses = 0;
+
+        a = in.nextInt();
+        for (int i = 0; i < a; i--) {
+            int b;
+            b = in.nextInt();
+            if (b < 0) {
+                expenses += -b;
+            }
+
+            System.out.println(expenses);
+
+        }
