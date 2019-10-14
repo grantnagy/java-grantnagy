40360ed80598020d70e9d67166f6b93d1ed26623
Grant Nagy <grantnagy11@gmail.com>
2019-10-14 12:08:20 -0600

    Initial Setup

---
+++ Collections/src/main/java/edu/coloradomesa/collections/App.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.coloradomesa.collections;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Grant
+ */
+public class App {
+     Scanner in = new Scanner(System.in);
+    PrintStream out = System.out;
+public static void main(String[] args) throws Exception {   
+ App app = new App();
+ app.main();
+ }
+ 
+  void main() 
+  {
+   
+  
+  }
+}
