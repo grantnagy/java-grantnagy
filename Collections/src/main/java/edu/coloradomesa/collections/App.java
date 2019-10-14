d661cfc00674468060a8c78056a3801dc0d9daa2
Grant Nagy <grantnagy11@gmail.com>
2019-10-14 12:45:29 -0600

    Hash setup

--- Collections/src/main/java/edu/coloradomesa/collections/App.java
+++ Collections/src/main/java/edu/coloradomesa/collections/App.java
@@ -6,23 +6,47 @@
 package edu.coloradomesa.collections;
 
 import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+
 
 /**
  *
  * @author Grant
  */
 public class App {
-     Scanner in = new Scanner(System.in);
+
+    Scanner in = new Scanner(System.in);
     PrintStream out = System.out;
-public static void main(String[] args) throws Exception {   
- App app = new App();
- app.main();
- }
- 
-  void main() 
-  {
+    Set<Integer> mySet = new HashSet<>();
    
-  
-  }
+
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.set();
+
+    }
+
+    void set() {
+
+        System.out.println(mySet.add(10));
+        System.out.println(mySet.add(11));
+        System.out.println(mySet.add(10));
+
+        mySet.add(11);
+        mySet.add(23);
+        mySet.add(39);
+        mySet.add(23);
+        mySet.add(40);
+        mySet.add(51);
+
+        System.out.println("Hashset:");
+
+        for (Integer anInt : mySet) {
+            System.out.println(anInt);
+        }
+    }
+
 }
