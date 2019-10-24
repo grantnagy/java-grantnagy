64de3ec7e7579e7ba9f5f92d66458d5c107b5e9c
grantnagy <50307885+grantnagy@users.noreply.github.com>
2019-10-23 21:04:36 -0600

    added time from start to finish and insync verison

--- threads/src/main/java/edu/coloradomesa/threads/app.java
+++ threads/src/main/java/edu/coloradomesa/threads/app.java
@@ -13,10 +13,25 @@ package edu.coloradomesa.threads;
 public class app {
 
     private int number = 0;
+    private int number2 = 0;
 
     public static void main(String[] args) {
         app app = new app();
+        double startTime = System.currentTimeMillis();
         app.generator();
+        double endTime = System.currentTimeMillis();
+        double diff = endTime - startTime;
+        System.out.println("Time from start to finish for desync: " + diff);
+        
+        /* in sync */
+        
+        double startTime2 = System.currentTimeMillis();
+        app.same();
+        double endTime2 = System.currentTimeMillis();
+        double diff2 = endTime2 - startTime2;
+        System.out.println("Time from start to finish for insync: " + diff2);
+        
+        
     }
 
     public void generator() {
@@ -45,4 +60,33 @@ public class app {
     public void subtract() {
         number--;
     }
+    
+
+    public void same() {
+            Thread t1 = new Thread(() -> {
+                for (int i = 0; i < 100000; i++)
+                    sadd();
+            });
+             Thread t2 = new Thread(() -> {
+                 for (int i = 0; i < 100000; i++)
+                     ssubtract();
+            });
+             
+             t1.start();
+             t2.start();
+             
+             try {
+                 t1.join();
+             }
+             catch (InterruptedException e) {
+             }
+             System.out.println("Number 2: " + number2);
+    }
+    public synchronized void sadd() {
+        number++;
+    }
+    public synchronized void ssubtract() {
+        number--;
+    }
+    
 }
