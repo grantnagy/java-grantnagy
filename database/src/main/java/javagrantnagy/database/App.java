052cf72675df073e596addee0aa79bcf73612b7e
grantnagy <50307885+grantnagy@users.noreply.github.com>
2019-10-29 13:49:15 -0600

    Successfully created and connected to db in one file

--- database/src/main/java/javagrantnagy/database/App.java
+++ database/src/main/java/javagrantnagy/database/App.java
@@ -9,18 +9,35 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+import java.sql.Statement;
 /**
  *
  * @author sqlitetutorial.net
  */
 public class App {
- 
-    /**
-     * Connect to a sample database
-     *
-     * @param fileName the database file name
-     */
+     private Connection connect() {
+        Connection conn = null;
+        try {
+           
+            String url = "jdbc:sqlite:C:/Users/Grant/Projects/java-grantnagy/database/sqlite/dbtest.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            
+        }
+      return conn;
+    }
+    
     public static void createNewDatabase(String fileName) {
  
         String url = "jdbc:sqlite:C:/Users/Grant/Projects/java-grantnagy/database/sqlite/db" + fileName;
@@ -36,11 +53,32 @@ public class App {
             System.out.println(e.getMessage());
         }
     }
- 
+
+ public void createNewTable() {
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = connect();
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        createNewDatabase("test.db");
+        new App().run();
+    
+    }
+    void run(){
+    createNewDatabase("test.db");
+    createNewTable();
     }
 }
