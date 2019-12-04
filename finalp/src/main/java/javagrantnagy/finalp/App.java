a6d2c9e790df412029a837932f3eeada84ded399
grantnagy <50307885+grantnagy@users.noreply.github.com>
2019-12-04 16:08:11 -0700

    Completed

--- finalp/src/main/java/javagrantnagy/finalp/App.java
+++ finalp/src/main/java/javagrantnagy/finalp/App.java
@@ -8,17 +8,19 @@ package javagrantnagy.finalp;
 import javax.swing.JOptionPane;
 
 public class App {
-        int small = 9, medium = 12, large = 15, cups = 0, amount = 0, small_t = 0, medium_t = 0, large_t = 0, total_cups;  //t for total
-        double dsmall = 1.75, dmedium = 1.90, dlarge = 2.00, cost, total_coffee, total_money;  //d for dollars
-        char sizeselect;
-        
+
+    int small = 9, medium = 12, large = 15, cups = 0, amount = 0, small_t = 0, medium_t = 0, large_t = 0, total_cups;  //t for total
+    double dsmall = 1.75, dmedium = 1.90, dlarge = 2.00, cost, total_coffee, total_money;  //d for dollars
+    char sizeselect;
+
     public static void main(String[] args) {
         App app = new App();
         app.menu();
     }
-void menu(){
 
-Object[] possibilities = {"Buy Coffee", "Total Cups Sold", "Total Money Made", "Exit"};
+    void menu() {
+
+        Object[] possibilities = {"Buy Coffee", "Total Cups Sold", "Total Money Made", "Total Ounces Sold"};
         String s = (String) JOptionPane.showInputDialog(
                 null,
                 "Welcome to the Coffee Shop!\n" + "Please select from the following menu: \n", "Coffee Shop",
@@ -37,11 +39,15 @@ Object[] possibilities = {"Buy Coffee", "Total Cups Sold", "Total Money Made", "
             case "Exit":
                 break;
             case "Total Money Made":
-               // show_total_money();
-            default:
-              //  exit;
+                show_total_money();
+                break;
+            case "Total Ounces Sold":
+                show_total_coffee();
+                break;
+
         }
-}
+    }
+
     void sell_coffee() {
         Object[] possibilities = {"Small: 9oz | $1.75", "Medium: 12 oz | $1.90", "Large: 15 oz | $2.00"};
 
@@ -52,52 +58,68 @@ Object[] possibilities = {"Buy Coffee", "Total Cups Sold", "Total Money Made", "
                 null,
                 possibilities,
                 possibilities[0]);
-        
-         System.out.println(cc);
-         
-         String amt = JOptionPane.showInputDialog("How many cups would you like?");
-         amount = Integer.parseInt(amt);
-         
-         switch (cc)
-	{
-	case "Small: 9oz | $1.75": if (amount >= 0) {
-		small_t = amount + small_t;
-		cost = small_t * dsmall;
-		bought();
-	}
-			else System.out.println("Error");
 
-		break;
-	case "Medium: 12 oz | $1.90": if (amount >= 0)
-	{
-		medium_t = amount + medium_t;
-		cost = medium_t * dmedium;
-		bought();
-	}
-			else System.out.println("Error");
-		break;
-	case "Large: 15 oz | $2.00": if (amount >= 0)
-	{
-		large_t = amount + large_t;
-		cost = large_t * dlarge;
-		bought();
-	}
-			else System.out.println("Error");
-		break;
-	default: System.out.println("Error");
-	}
-         
+        System.out.println(cc);
+
+        String amt = JOptionPane.showInputDialog("How many cups would you like?");
+        amount = Integer.parseInt(amt);
+
+        switch (cc) {
+            case "Small: 9oz | $1.75":
+                if (amount >= 0) {
+                    small_t = amount + small_t;
+                    cost = small_t * dsmall;
+                    bought();
+                } else {
+                    System.out.println("Error");
+                }
+
+                break;
+            case "Medium: 12 oz | $1.90":
+                if (amount >= 0) {
+                    medium_t = amount + medium_t;
+                    cost = medium_t * dmedium;
+                    bought();
+                } else {
+                    System.out.println("Error");
+                }
+                break;
+            case "Large: 15 oz | $2.00":
+                if (amount >= 0) {
+                    large_t = amount + large_t;
+                    cost = large_t * dlarge;
+                    bought();
+                } else {
+                    System.out.println("Error");
+                }
+                break;
+            default:
+                System.out.println("Error");
+        }
+
     }
-    void bought(){
+
+    void bought() {
         JOptionPane.showMessageDialog(null, "You have bought " + amount + " cups of coffee for $" + cost);
         menu();
     }
-    void show_total_cups()
-    {
+
+    void show_total_cups() {
         total_cups = small_t + medium_t + large_t;
-        JOptionPane.showMessageDialog(null, "There have been " + small_t + " small cups sold, " + medium_t + " medium cups sold, and " 
+        JOptionPane.showMessageDialog(null, "There have been " + small_t + " small cups sold, " + medium_t + " medium cups sold, and "
                 + large_t + " large cups sold." + "\n" + "Total: " + total_cups + " Cups");
         menu();
     }
-    
+
+    void show_total_coffee() {
+        total_coffee = (small_t * small) + (medium_t * medium) + (large_t * large);
+        JOptionPane.showMessageDialog(null, "Total Ounces of Coffee Sold: " + total_coffee);
+        menu();
+    }
+
+    void show_total_money() {
+        total_money = (small_t * dsmall) + (medium_t * dmedium) + (large_t * dlarge);
+        JOptionPane.showMessageDialog(null, "Total Dollars of Coffee Sold: $" + total_money);
+        menu();
+    }
 }
