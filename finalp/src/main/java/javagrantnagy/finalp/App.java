187d047fb6921e7eacc85cf577325153e64b5e7f
grantnagy <50307885+grantnagy@users.noreply.github.com>
2019-12-04 16:39:27 -0700

    added exit even though theres a cancel button

--- finalp/src/main/java/javagrantnagy/finalp/App.java
+++ finalp/src/main/java/javagrantnagy/finalp/App.java
@@ -11,7 +11,6 @@ public class App {
 
     int small = 9, medium = 12, large = 15, cups = 0, amount = 0, small_t = 0, medium_t = 0, large_t = 0, total_cups;  //t for total
     double dsmall = 1.75, dmedium = 1.90, dlarge = 2.00, cost, total_coffee, total_money;  //d for dollars
-    char sizeselect;
 
     public static void main(String[] args) {
         App app = new App();
@@ -20,7 +19,7 @@ public class App {
 
     void menu() {
 
-        Object[] possibilities = {"Buy Coffee", "Total Cups Sold", "Total Money Made", "Total Ounces Sold"};
+        Object[] possibilities = {"Buy Coffee", "Total Cups Sold", "Total Money Made", "Total Ounces Sold", "Exit"};
         String s = (String) JOptionPane.showInputDialog(
                 null,
                 "Welcome to the Coffee Shop!\n" + "Please select from the following menu: \n", "Coffee Shop",
@@ -44,7 +43,7 @@ public class App {
             case "Total Ounces Sold":
                 show_total_coffee();
                 break;
-
+        
         }
     }
 
