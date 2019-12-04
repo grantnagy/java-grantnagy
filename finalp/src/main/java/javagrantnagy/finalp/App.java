8b8f2b5812c0de9b70c44d22d3ada5bb939b3c25
grantnagy <50307885+grantnagy@users.noreply.github.com>
2019-12-04 13:16:53 -0700

    working test

--- finalp/src/main/java/javagrantnagy/finalp/App.java
+++ finalp/src/main/java/javagrantnagy/finalp/App.java
@@ -25,37 +25,32 @@ import org.jsoup.select.Elements;
  */
 public class App {
     
-    private static final String WEBSITE = "https://www.google.com/search?q=nfl&rlz=1C1CHBF_enUS869US869&oq=nfl&aqs=chrome..69i57j69i59l2j69i60l3.675j0j7&sourceid=chrome&ie=UTF-8#sie=lg;/g/11ffv6kjj0;6;/m/059yj;st;fp;1;;";
+    private static final String WEBSITE = "https://www.revengeofficial.com";
     
     private Map<String, String> teams;
     
     public static void main(String[] args) {
         App app = new App();
-       app.Main();
+       app.scanTeams();
     }
     
-    
-    void Main() {
+    private void scanTeams(){
         teams = new HashMap<>();
-        scanTeams();
-    }
-   
-    void scanTeams(){
         Document document;
         
         try{
-            document = Jsoup.connect(WEBSITE).get();
+            document = Jsoup.connect(WEBSITE + "/webstore").get();
         } catch (IOException ignored) {
             System.out.println("Could not show teams");
             return;
         }
         
-        Elements elements = document.getElementsByClass("liveresults-sports-immersive__str-dc liveresults-sports-immersive__st-sc");
+        Elements elements = document.getElementsByClass("ProductList-item-link");
         
         for (Element element : elements) {
-            String link = element.attributes().get("td");
+            String link = element.attributes().get("href");
             
-            System.out.println("link");
+            System.out.println(link);
         }
     }
     
